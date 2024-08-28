7ce0809e29326d861ad9609bcb18325d62d83a3d
Seizal <guillaumelazies@gmail.com>
2024-08-28 11:23:44 +0200

    first commit

--- QuizData/QuizData/Models/Cars.cs
+++ QuizData/QuizData/Models/Cars.cs
@@ -4,7 +4,9 @@ namespace QuizData.Models
 {
     public class Car
     {
+         // Déclaration de la propriété Name de type string
         public string Name { get; set; }
+        // Déclaration de la propriété Group de type string
         public string Group { get; set; }
     }
 }
--- QuizData/QuizData/Models/CarsGroup.cs
+++ QuizData/QuizData/Models/CarsGroup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace QuizData.Models
 {
+    // Déclaration de la classe CarGroup et de ses propriété
     public class CarGroup
     {
         public string GroupName { get; set; }
